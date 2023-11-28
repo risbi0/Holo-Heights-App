6716072c6d4f8e675e1e490bb13fed67e87d0c52
risbi0 <81689588+risbi0@users.noreply.github.com>
2023-11-28 20:55:21 +0800

    add nijisanji talents

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -7,7 +7,7 @@ function Notes() {
 				className='block w-20 hover:font-bold font-medium rounded-lg text-sm px-4 py-2 text-center bg-gray-900 hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-gray-700'
 				type='button'
 			>
-				Notes
+				About
 			</button>
 			<div
 				id='default-modal'
@@ -16,9 +16,9 @@ function Notes() {
 				className='hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full sm:inset-0 h-[calc(100%-1rem)] max-h-full'
 			>
 				<div className='relative p-4 w-full max-w-md max-h-full'>
-					<div className='relative bg-gray-700 rounded-lg shadow'>
+					<div className='relative flex flex-col bg-gray-700 rounded-lg shadow h-96'>
 						<div className='flex items-center justify-between py-3 px-4 border-gray-600 border-b rounded-t'>
-							<h3 className='text-xl font-semibold'>Notes</h3>
+							<h3 className='text-xl font-semibold'>About</h3>
 							<button
 								type='button'
 								className='bg-transparent hover:bg-gray-300 hover:text-gray-700 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center'
@@ -30,15 +30,36 @@ function Notes() {
 								<span className='sr-only'>Close modal</span>
 							</button>
 						</div>
-						<div className='p-4 sm:p-5 space-y-4'>
+						<div className='p-4 sm:p-5 space-y-4 overflow-y-scroll'>
+							<p>Compare Hololive and Nijisanji talents scaled to their heights. Includes ALL talents in Hololive and Nijisanji (up to November 2023) that satisfies all of the following conditions: a full body avatar with transparent background available in the sites mentioned below; and an exact height within the range of human measurement.</p>
+
+							<p>Notes:</p>
 							<p className='text-base leading-relaxed list-item ml-4'>
-								Height is assumed to be the measure between the bottom of the soles/feet and the top of the head. Guesswork was done to some talents with hats.
+								Height is assumed to be the measure between the bottom of the soles/feet and the top of the head. Guesswork was done to some talents with obstructions on top of their head.
+							</p>
+							<p className='text-base leading-relaxed list-item ml-4'>
+								If the height has a decimal, it is rounded to the nearest whole number. Imperial conversion of heights are rounded to the nearest inch.
+							</p>
+							<p className='text-base leading-relaxed list-item ml-4'>
+								Height data for Hololive Production talents is based on the official websites for <a className="underline" href="https://hololive.hololivepro.com/en/talents/" target="_blank" rel="noreferrer">Hololive</a> and <a className="underline" href="https://holostars.hololivepro.com/talent" target="_blank" rel="noreferrer">Holostars</a>. An exception for Banzoin Hakka where I used the height that includes the shoes, which I got from <a className="underline" href="https://virtualyoutuber.fandom.com/wiki/" target="_blank" rel="noreferrer">Virtual Youtuber Wiki</a>. For Nijisanji talents, I used the same website. If there&apos;s no height displayed, I checked the <a className="underline" href="https://wikiwiki.jp/nijisanji/" target="_blank" rel="noreferrer">Nijisanji JP wiki</a> if there is one. If the height specifies including the heels/shoes, that one is used.
 							</p>
 							<p className='text-base leading-relaxed list-item ml-4'>
 								Heights of 3D avatars are less consistently represented compared to 2D since they&apos;re not standing as straight.
 							</p>
+
+							<p>The following talents are ones that I modified their height due to various reasons:</p>
 							<p className='text-base leading-relaxed list-item ml-4'>
-								Imperial conversion of heights are rounded to the nearest inch.
+								Ike Eveland - Added 7 centimeters. Acquired height specifies not including the heels.
+							</p>
+							<p className='text-base leading-relaxed list-item ml-4'>
+								Mononobe Alice - Removed 15 centemers. Acquired height includes ribbons.
+							</p>
+							<p className='text-base leading-relaxed list-item ml-4'>
+								Naraka - Added 6 centimeters. The other height includes shoes AND horns. No height including shoes only.
+							</p>
+
+							<p>
+								Any wrong/updated heights, updated default models, missing talents that satisfy the aforementioned conditions, contact me in Discord (username: risbiantotri).
 							</p>
 						</div>
 					</div>
--- src/data.js
+++ src/data.js
@@ -371,4 +371,828 @@ export const heightData = {
 		height: 184,
 		color: 'E70013'
 	},
+	'Aia Amare': {
+		height: 158,
+		color: 'FFFEF7'
+	},
+	'Alban Knox': {
+		height: 175,
+		color: 'FF5F00'
+	},
+	'Aster Arcadia': {
+		height: 170,
+		color: '6662A4'
+	},
+	'Claude Clawmark': {
+		height: 171,
+		color: '7D3CAF'
+	},
+	'Doppio Dropscythe': {
+		height: 187,
+		color: 'A50082'
+	},
+	'Elira Pendora': {
+		height: 160,
+		color: '95C8D8'
+	},
+	'Finana Ryugu': {
+		height: 140,
+		color: '79CFB8'
+	},
+	'Fulgur Ovid': {
+		height: 178,
+		color: 'FF0000'
+	},
+	'Hex Haywire': {
+		height: 185,
+		color: '007199'
+	},
+	'Ike Eveland': {
+		height: 180,
+		color: '348EC7'
+	},
+	'Kotoka Torahime': {
+		height: 166,
+		color: 'DC6B9A'
+	},
+	'Kunai Nakasato': {
+		height: 162,
+		color: '3C6E64'
+	},
+	'Kyo Kaneko': {
+		height: 183,
+		color: '00AFCC'
+	},
+	'Luca Kaneshiro': {
+		height: 178,
+		color: 'D4AF37'
+	},
+	'Maria Marionette': {
+		height: 152,
+		color: 'E55A98'
+	},
+	'Meloco Kyoran': {
+		height: 171,
+		color: 'A09BD8'
+	},
+	'Millie Parfait': {
+		height: 150,
+		color: 'FEBC87'
+	},
+	'Mysta Rias': {
+		height: 177,
+		color: 'C3552B'
+	},
+	'Nina Kosaka': {
+		height: 174,
+		color: '660000'
+	},
+	'Petra Gurin': {
+		height: 152,
+		color: 'FFAE42'
+	},
+	'Pomu Rainpuff': {
+		height: 156,
+		color: '258E70'
+	},
+	'Reimu Endou': {
+		height: 155,
+		color: 'B90B4A'
+	},
+	'Ren Zotto': {
+		height: 180,
+		color: '429B76'
+	},
+	'Rosemi Lovelock': {
+		height: 150,
+		color: 'FFC0CB'
+	},
+	'Scarle Yonaguni': {
+		height: 165,
+		color: 'E60012'
+	},
+	'Selen Tatsuki': {
+		height: 160,
+		color: '7E4EAC'
+	},
+	'Shu Yamino': {
+		height: 173,
+		color: 'A660A7'
+	},
+	'Sonny Brisko': {
+		height: 180,
+		color: 'FFF321'
+	},
+	'Uki Violeta': {
+		height: 169,
+		color: 'B600FF'
+	},
+	'Vantacrow Bringer': {
+		height: 182,
+		color: '5E2DA2'
+	},
+	'Ver Vermillion': {
+		height: 175,
+		color: 'D5345E'
+	},
+	'Vezalius Bandage': {
+		height: 178,
+		color: 'EECBB7'
+	},
+	'Victoria Brightshield': {
+		height: 157,
+		color: 'D7D2EB'
+	},
+	'Vox Akuma': {
+		height: 178,
+		color: '960018'
+	},
+	'Yugo Asuma': {
+		height: 168,
+		color: '1F51FF'
+	},
+	'Yu Q Wilson': {
+		height: 175,
+		color: 'E5D961'
+	},
+	'Zaion LanZa': {
+		height: 153,
+		color: 'FFF462'
+	},
+	'Achikita Chinami': {
+		height: 154,
+		color: 'FCC5DC'
+	},
+	'Aiba Uiha': {
+		height: 167,
+		color: '324CAC'
+	},
+	'Aizono Manami': {
+		height: 154,
+		color: 'F98FB7'
+	},
+	'Akabane Youko': {
+		height: 160,
+		color: 'CA636C'
+	},
+	'Akagi Wen': {
+		height: 171,
+		color: 'FF8C9B'
+	},
+	'Akira Ray': {
+		height: 155,
+		color: 'A9CEEC'
+	},
+	'Amagase Muyu': {
+		height: 167,
+		color: 'FA5578'
+	},
+	'Amamiya Kokoro': {
+		height: 150,
+		color: 'C5EDFF'
+	},
+	'Amemori Sayo': {
+		height: 157,
+		color: '756F7D'
+	},
+	'Amicia Michella': {
+		height: 150,
+		color: '45E101'
+	},
+	'Ange Katrina': {
+		height: 160,
+		color: 'C83C35'
+	},
+	'Ars Almal': {
+		height: 147,
+		color: '7FD6E2'
+	},
+	'Asahina Akane': {
+		height: 158,
+		color: 'B7282E'
+	},
+	'Asuka Hina': {
+		height: 145,
+		color: 'FAD8DC'
+	},
+	'Axia Krone': {
+		height: 179,
+		color: '00A6FE'
+	},
+	'Azuchi Momo': {
+		height: 154,
+		color: 'F295CE'
+	},
+	'Azura Cecillia': {
+		height: 171,
+		color: '1B486F'
+	},
+	'Ban Hada': {
+		height: 165,
+		color: '96CEEB'
+	},
+	'Belmond Banderas': {
+		height: 191,
+		color: '683D46'
+	},
+	'Bonnivier Pranaja': {
+		height: 183,
+		color: '00FFCD'
+	},
+	'Derem Kado': {
+		height: 156,
+		color: 'F6D4DF'
+	},
+	'Dola': {
+		height: 170,
+		color: 'A83E4A'
+	},
+	'Eli Conifer': {
+		height: 155,
+		color: 'DAFFF9'
+	},
+	'Elu': {
+		height: 170,
+		color: 'E2364F'
+	},
+	'Emma August': {
+		height: 138,
+		color: 'B32F51'
+	},
+	'Etna Crimson': {
+		height: 154,
+		color: 'DC143C'
+	},
+	'Ex Albio': {
+		height: 183,
+		color: '5C9BBC'
+	},
+	'Fumino Tamaki': {
+		height: 157,
+		color: 'EBDDB4'
+	},
+	'Fumi': {
+		height: 176,
+		color: 'F4E49D'
+	},
+	'Fura Kanato': {
+		height: 177,
+		color: 'F0F0F5'
+	},
+	'Furen E Lustario': {
+		height: 168,
+		color: 'EC1D2F'
+	},
+	'Fushimi Gaku': {
+		height: 178,
+		color: 'FFCB5B'
+	},
+	'Fuwa Minato': {
+		height: 173,
+		color: 'BF69F4'
+	},
+	'Gaon': {
+		height: 170,
+		color: 'F9E072'
+	},
+	'Genzuki Tojiro': {
+		height: 183,
+		color: '487591'
+	},
+	'Gundo Mirei': {
+		height: 175,
+		color: 'A20063'
+	},
+	'Gwelu Os Gar': {
+		height: 185,
+		color: 'F04B2D'
+	},
+	'Hakase Fuyuki': {
+		height: 152,
+		color: 'EEFFFF'
+	},
+	'Hanabatake Chaika': {
+		height: 186,
+		color: '40CF84'
+	},
+	'Hana Macchia': {
+		height: 158,
+		color: 'FFEFD5'
+	},
+	'Han Chiho': {
+		height: 178,
+		color: '645E5D'
+	},
+	'Harusaki Air': {
+		height: 180,
+		color: '4B5F9E'
+	},
+	'Hassaku Yuzu': {
+		height: 155,
+		color: 'FCCA4D'
+	},
+	'Hayama Marin': {
+		height: 158,
+		color: 'D9EC33'
+	},
+	'Hayase Sou': {
+		height: 170,
+		color: 'AA7BE8'
+	},
+	'Ha Yun': {
+		height: 178,
+		color: 'FFF9B0'
+	},
+	'Hibachi Mana': {
+		height: 170,
+		color: '64C8F0'
+	},
+	'Higuchi Kaede': {
+		height: 167,
+		color: 'FBAF71'
+	},
+	'Hoshikawa Sara': {
+		height: 155,
+		color: 'FAB80D'
+	},
+	'Hoshirube Sho': {
+		height: 177,
+		color: 'A58CDC'
+	},
+	'Hyakumantenbara Salome': {
+		height: 163,
+		color: 'CD3896'
+	},
+	'Hyona Elatiora': {
+		height: 165,
+		color: 'FDBEDF'
+	},
+	'Ibrahim': {
+		height: 177,
+		color: '7CA1F0'
+	},
+	'Ienaga Mugi': {
+		height: 154,
+		color: 'FF899D'
+	},
+	'Igarashi Rika': {
+		height: 152,
+		color: 'FF8C3C'
+	},
+	'Inami Rai': {
+		height: 169,
+		color: 'DCFF50'
+	},
+	'Inui Toko': {
+		height: 162,
+		color: '9D3757'
+	},
+	'Ishigami Nozomi': {
+		height: 157,
+		color: 'E6325F'
+	},
+	'Izumo Kasumi': {
+		height: 158,
+		color: 'A9D6E3'
+	},
+	'Joe Rikiichi': {
+		height: 185,
+		color: 'D598DD'
+	},
+	'Kaburaki Roco': {
+		height: 165,
+		color: '198CAA'
+	},
+	'Kagami Hayato': {
+		height: 182,
+		color: 'B9ADB9'
+	},
+	'Kaida Haru': {
+		height: 183,
+		color: '4DD7E3'
+	},
+	'Kanae': {
+		height: 175,
+		color: 'CA636C'
+	},
+	'Kanda Shoichi': {
+		height: 175,
+		color: 'F4D35B'
+	},
+	'Kataribe Tsumugu': {
+		height: 165,
+		color: '07A4E3'
+	},
+	'Kenmochi Toya': {
+		height: 172,
+		color: 'A590AF'
+	},
+	'Kingyozaka Meiro': {
+		height: 145,
+		color: 'FED5D7'
+	},
+	'Kitakoji Hisui': {
+		height: 153,
+		color: '38B48B'
+	},
+	'Koshimizu Toru': {
+		height: 159,
+		color: 'CDA5FF'
+	},
+	'Koyanagi Rou': {
+		height: 173,
+		color: '5A5FAA'
+	},
+	'Kudo Chitose': {
+		height: 159,
+		color: 'FBD5F1'
+	},
+	'Kuramochi Meruto': {
+		height: 154,
+		color: 'EB4682'
+	},
+	'Kurusu Natsume': {
+		height: 171,
+		color: '880223'
+	},
+	'Kuzuha': {
+		height: 178,
+		color: 'ACA7BB'
+	},
+	'Lain Paterson': {
+		height: 163,
+		color: 'F74848'
+	},
+	'Lauren Iroas': {
+		height: 176,
+		color: 'C10E49'
+	},
+	'Layla Alstroemeria': {
+		height: 152,
+		color: 'F78181'
+	},
+	'Lee Roha': {
+		height: 165,
+		color: 'FF2649'
+	},
+	'Leos Vincent': {
+		height: 180,
+		color: '234A87'
+	},
+	'Levi Elipha': {
+		height: 165,
+		color: 'E8E1E8'
+	},
+	'Lize Helesta': {
+		height: 158,
+		color: '42FFFF'
+	},
+	'Luis Cammy': {
+		height: 168,
+		color: 'E86A74'
+	},
+	'Maimoto Keisuke': {
+		height: 178,
+		color: '094078'
+	},
+	'Makaino Ririmu': {
+		height: 138,
+		color: 'EF6F94'
+	},
+	'Mashiro Meme': {
+		height: 162,
+		color: '1E2232'
+	},
+	'Matsukai Mao': {
+		height: 142,
+		color: 'C93965'
+	},
+	'Mayuzumi Kai': {
+		height: 178,
+		color: '086776'
+	},
+	'Melissa Kinrenka': {
+		height: 161,
+		color: 'D3F377'
+	},
+	'Mika Melatika': {
+		height: 155,
+		color: 'DDBFF8'
+	},
+	'Milan Kestrel': {
+		height: 178,
+		color: '5A5FAA'
+	},
+	'Min Suha': {
+		height: 180,
+		color: '5AC8FF'
+	},
+	'Miyu Ottavia': {
+		height: 155,
+		color: 'FFAB75'
+	},
+	'Moira': {
+		height: 165,
+		color: '91ABD0'
+	},
+	'Mononobe Alice': {
+		height: 128,
+		color: '81D4E2'
+	},
+	'Morinaka Kazaki': {
+		height: 135,
+		color: 'C8F39A'
+	},
+	'Murakumo Kagetsu': {
+		height: 169,
+		color: '6EB487'
+	},
+	'Nagao Kei': {
+		height: 177,
+		color: '625DA1'
+	},
+	'Nagisa Arcinia': {
+		height: 134,
+		color: 'CC338B'
+	},
+	'Nakao Azuma': {
+		height: 154,
+		color: 'F7A57D'
+	},
+	'Naraka': {
+		height: 144,
+		color: 'A02655'
+	},
+	'Nara Haramaung': {
+		height: 173,
+		color: 'FEA191'
+	},
+	'Naruse Naru': {
+		height: 168,
+		color: 'E06489'
+	},
+	'Na Sera': {
+		height: 155,
+		color: 'FEECF3'
+	},
+	'Nishizono Chigusa': {
+		height: 157,
+		color: '3A8FB7'
+	},
+	'Nui Sociere': {
+		height: 165,
+		color: 'D24E5F'
+	},
+	'Oh Jiyu': {
+		height: 171,
+		color: 'F1F4F4'
+	},
+	'Oliver Evans': {
+		height: 190,
+		color: 'BCC37E'
+	},
+	'Onomachi Haruka': {
+		height: 154,
+		color: 'FF336E'
+	},
+	'Otogibara Era': {
+		height: 155,
+		color: 'FFBE5C'
+	},
+	'Ponto Nei': {
+		height: 155,
+		color: '5A7DFF'
+	},
+	'Rai Galilei': {
+		height: 158,
+		color: '7DF9FF'
+	},
+	'Ratna Petit': {
+		height: 156,
+		color: 'F8B759'
+	},
+	'Reza Avanluna': {
+		height: 175,
+		color: 'FEA191'
+	},
+	'Riksa Dhirendra': {
+		height: 178,
+		color: 'FF0103'
+	},
+	'Ryushen': {
+		height: 164,
+		color: '5DCCAB'
+	},
+	'Ryu Hari': {
+		height: 157,
+		color: '96CEEB'
+	},
+	'Saegusa Akina': {
+		height: 168,
+		color: 'F03C31'
+	},
+	'Saiki Ittetsu': {
+		height: 174,
+		color: '8C4664'
+	},
+	'Sakura Ritsuki': {
+		height: 165,
+		color: 'C57FC7'
+	},
+	'Sasaki Saku': {
+		height: 148,
+		color: 'EF9AAF'
+	},
+	'Seffyna': {
+		height: 168,
+		color: 'FFBDC9'
+	},
+	'Seraph Dazzlegarden': {
+		height: 186,
+		color: 'DC3C41'
+	},
+	'Seto Miyako': {
+		height: 158,
+		color: 'C4FF2B'
+	},
+	'Shellin Burgundy': {
+		height: 183,
+		color: '6C2735'
+	},
+	'Shibuya Hajime': {
+		height: 180,
+		color: 'D7FAD7'
+	},
+	'Shikinagi Akira': {
+		height: 174,
+		color: '235AAA'
+	},
+	'Shioriha Ruri': {
+		height: 160,
+		color: 'A58CDC'
+	},
+	'Shirayuki Tomoe': {
+		height: 178,
+		color: '6E3FE7'
+	},
+	'Shishido Akari': {
+		height: 149,
+		color: 'FFD728'
+	},
+	'Shizuka Rin': {
+		height: 158,
+		color: '745399'
+	},
+	'Siska Leontyne': {
+		height: 170,
+		color: 'FFDFA0'
+	},
+	'Sister Cleaire': {
+		height: 150,
+		color: 'DFC6A8'
+	},
+	'Song Mia': {
+		height: 159,
+		color: 'FFF9B0'
+	},
+	'Sophia Valentine': {
+		height: 153,
+		color: 'C8C3DC'
+	},
+	'Sorahoshi Kirame': {
+		height: 148,
+		color: '44DDF4'
+	},
+	'So Nagi': {
+		height: 160,
+		color: '7C86DE'
+	},
+	'Sukoya Kana': {
+		height: 167,
+		color: 'FF2FA2'
+	},
+	'Suo Sango': {
+		height: 148,
+		color: 'EF8468'
+	},
+	'Suzuhara Lulu': {
+		height: 162,
+		color: 'D7AFA8'
+	},
+	'Suzuka Utako': {
+		height: 159,
+		color: 'FA4F62'
+	},
+	'Suzuki Masaru': {
+		height: 153,
+		color: '7B788A'
+	},
+	'Suzuya Aki': {
+		height: 155,
+		color: 'F2F9C3'
+	},
+	'Tachitsute Toto': {
+		height: 154,
+		color: '6EB487'
+	},
+	'Takamiya Rion': {
+		height: 158,
+		color: 'CC3D7B'
+	},
+	'Taka Radjiman': {
+		height: 175,
+		color: '303764'
+	},
+	'Todoroki Kyoko': {
+		height: 165,
+		color: 'F8A69A'
+	},
+	'Todo Kohaku': {
+		height: 162,
+		color: 'EA930A'
+	},
+	'Tsukimi Shizuku': {
+		height: 149,
+		color: 'E354AF'
+	},
+	'Tsukino Mito': {
+		height: 151,
+		color: 'E43F3B'
+	},
+	'Umise Yotsuha': {
+		height: 163,
+		color: 'FFE632'
+	},
+	'Usami Rito': {
+		height: 181,
+		color: 'FFD728'
+	},
+	'Ushimi Ichigo': {
+		height: 125,
+		color: 'FFCACE'
+	},
+	'Uzuki Kou': {
+		height: 163,
+		color: 'F9E97A'
+	},
+	'Warabeda Meiji': {
+		height: 147,
+		color: 'E34E4F'
+	},
+	'Watarai Hibari': {
+		height: 183,
+		color: '192332'
+	},
+	'Xia Ekavira': {
+		height: 169,
+		color: 'FFF9B0'
+	},
+	'Yaguruma Rine': {
+		height: 141,
+		color: 'FEECD8'
+	},
+	'Yamagami Karuta': {
+		height: 158,
+		color: '384B5A'
+	},
+	'Yamiyono Moruru': {
+		height: 162,
+		color: 'EC4676'
+	},
+	'Yang Nari': {
+		height: 142,
+		color: 'FFF8C8'
+	},
+	'Yashiro Kizuku': {
+		height: 180,
+		color: 'B6C6F2'
+	},
+	'Yorumi Rena': {
+		height: 154,
+		color: 'F7265A'
+	},
+	'Yukishiro Mahiro': {
+		height: 152,
+		color: 'B4E9FF'
+	},
+	'Yumeoi Kakeru': {
+		height: 175,
+		color: 'AC324B'
+	},
+	'Yuuhi Riri': {
+		height: 162,
+		color: 'BFFFFF'
+	},
+	'Yuki Chihiro': {
+		height: 138,
+		color: '7BB3EE'
+	},
+	'Yuzuki Roa': {
+		height: 141,
+		color: 'D8368D'
+	},
+	'ZEA Cornelia': {
+		height: 149,
+		color: 'FBEC5D'
+	},
 }
