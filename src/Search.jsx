6b7dfd505d8d7a1652512aeb85176d8b6909428c
risbi0 <81689588+risbi0@users.noreply.github.com>
2023-11-29 09:34:11 +0800

    make search case insensitive

--- src/Search.jsx
+++ src/Search.jsx
@@ -2,7 +2,7 @@ function Search({setSearchStr}) {
 	return (
 		<input
 			type='search'
-			onChange={(e) => setSearchStr(e.target.value)}
+			onChange={(e) => setSearchStr(e.target.value.toLowerCase())}
 			className='w-9/12 sm:w-80 font-bold my-3 py-2 px-4 bg-gray-600 rounded-2xl border-4 border-gray-700 outline-none text-center'
 			placeholder='Search'
 		/>
