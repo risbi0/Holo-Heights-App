f1e454035a6cfdbc32ba1810aa1b35fd1d82dc2b
risbi0 <81689588+risbi0@users.noreply.github.com>
2023-12-16 20:34:46 +0800

    add indent

--- src/App.jsx
+++ src/App.jsx
@@ -178,116 +178,116 @@ function App() {
 					transform: window.innerWidth >= 640 ? `translate(calc(-50% + 0px), calc(-50% + 0px))` : ''
 				}}
 			>
-			<div
-				className='flex flex-col items-center w-full h-full sm:pt-5 overflow-y-auto'
-			>
-				{/* selected */}
-				{Object.entries(selected).length === 0
-					?	<div className='italic text-lg my-2 sm:mt-4'>select a member</div>
-					: <>
-						<div className='flex flex-col items-center w-[90%] h-full sm:max-w-[600px]'>
-							{Object.keys(selected).map((name, index) => (
-								<div
-									key={index}
-									className='flex justify-between items-center w-full flex-col sm:flex-row gap-2 sm:gap-4 bg-gray-900 my-2 px-4 py-2 rounded-2xl text-lg overflow-x-auto'
-								>
-									<div className='flex justify-evenly sm:justify-start items-center w-full sm:w-1/2'>
-										<div
-											className='w-10 h-10 mr-2 bg-contain rounded-full border-2 bg-no-repeat bg-center'
-											style={{'backgroundImage': `url(./${name.replaceAll(' ', '%20')}.png)`, 'borderColor': `#${heightData[name]['color']}`}}
-										></div>
-										<div className='flex grow justify-around sm:justify-between w-auto sm:w-[inherit]'>
-											<div className='font-bold whitespace-nowrap text-ellipsis overflow-hidden'>{name}</div>
-											<div className='font-bold'>{!imperialConv ? heightData[name]['height'] : cmToFeetAndInches(heightData[name]['height'])}</div>
+				<div
+					className='flex flex-col items-center w-full h-full sm:pt-5 overflow-y-auto'
+				>
+					{/* selected */}
+					{Object.entries(selected).length === 0
+						?	<div className='italic text-lg my-2 sm:mt-4'>select a member</div>
+						: <>
+							<div className='flex flex-col items-center w-[90%] h-full sm:max-w-[600px]'>
+								{Object.keys(selected).map((name, index) => (
+									<div
+										key={index}
+										className='flex justify-between items-center w-full flex-col sm:flex-row gap-2 sm:gap-4 bg-gray-900 my-2 px-4 py-2 rounded-2xl text-lg overflow-x-auto'
+									>
+										<div className='flex justify-evenly sm:justify-start items-center w-full sm:w-1/2'>
+											<div
+												className='w-10 h-10 mr-2 bg-contain rounded-full border-2 bg-no-repeat bg-center'
+												style={{'backgroundImage': `url(./${name.replaceAll(' ', '%20')}.png)`, 'borderColor': `#${heightData[name]['color']}`}}
+											></div>
+											<div className='flex grow justify-around sm:justify-between w-auto sm:w-[inherit]'>
+												<div className='font-bold whitespace-nowrap text-ellipsis overflow-hidden'>{name}</div>
+												<div className='font-bold'>{!imperialConv ? heightData[name]['height'] : cmToFeetAndInches(heightData[name]['height'])}</div>
+											</div>
+										</div>
+										<div className='flex justify-evenly sm:justify-between items-center w-full sm:w-[40%]'>
+											{/* bring front/send back buttons */}
+											<button
+												className='flex justify-center items-center w-5 h-5 bg-contain bg-no-repeat bg-center'
+												onClick={() => updateZIndex(name, true)}
+												style={{'backgroundImage': `url(${bringFront})`}}
+											>
+											</button>
+											<button
+												className='flex justify-center items-center w-5 h-5 bg-contain bg-no-repeat bg-center'
+												onClick={() => updateZIndex(name, false)}
+												style={{'backgroundImage': `url(${sendBack})`}}
+											>
+											</button>
+											{/* move image buttons */}
+											<button
+												className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
+												onClick={() => moveImage(name, -5)}
+											>
+												<span className='material-symbols-outlined'>arrow_back</span>
+											</button>
+											<button
+												className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
+												onClick={() => moveImage(name, 5)}
+											>
+												<span className='material-symbols-outlined'>arrow_forward</span>
+											</button>
+											{/* remove button */}
+											<button
+												className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
+												onClick={() => updateSelection(name, false)}
+											>
+												<span className='material-symbols-outlined'>close</span>
+											</button>
 										</div>
 									</div>
-									<div className='flex justify-evenly sm:justify-between items-center w-full sm:w-[40%]'>
-										{/* bring front/send back buttons */}
-										<button
-											className='flex justify-center items-center w-5 h-5 bg-contain bg-no-repeat bg-center'
-											onClick={() => updateZIndex(name, true)}
-											style={{'backgroundImage': `url(${bringFront})`}}
-										>
-										</button>
-										<button
-											className='flex justify-center items-center w-5 h-5 bg-contain bg-no-repeat bg-center'
-											onClick={() => updateZIndex(name, false)}
-											style={{'backgroundImage': `url(${sendBack})`}}
-										>
-										</button>
-										{/* move image buttons */}
-										<button
-											className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
-											onClick={() => moveImage(name, -5)}
-										>
-											<span className='material-symbols-outlined'>arrow_back</span>
-										</button>
-										<button
-											className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
-											onClick={() => moveImage(name, 5)}
-										>
-											<span className='material-symbols-outlined'>arrow_forward</span>
-										</button>
-										{/* remove button */}
-										<button
-											className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
-											onClick={() => updateSelection(name, false)}
-										>
-											<span className='material-symbols-outlined'>close</span>
-										</button>
-									</div>
+								))}
+							</div>
+						</>
+					}
+					<hr className='w-[90%] sm:max-w-[650px] my-4 border-[1px] border-gray-300'/>
+					{/* not selected */}
+					<div className='flex flex-col items-center w-[90%] sm:max-w-[600px]'>
+						<div className='flex flex-col sm:flex-row justify-around items-center w-full gap-2 sm:gap-0'>
+							<div className='flex items-center'>
+								<div className='checkbox-wrapper mr-2'>
+									<input type='checkbox' id='toggle-chart' />
+									<label htmlFor='toggle-chart' className='toggle' onClick={() => setShowChartLines(!showChartLines)}><span></span></label>
 								</div>
-							))}
-						</div>
-					</>
-				}
-				<hr className='w-[90%] sm:max-w-[650px] my-4 border-[1px] border-gray-300'/>
-				{/* not selected */}
-				<div className='flex flex-col items-center w-[90%] sm:max-w-[600px]'>
-					<div className='flex flex-col sm:flex-row justify-around items-center w-full gap-2 sm:gap-0'>
-						<div className='flex items-center'>
-							<div className='checkbox-wrapper mr-2'>
-								<input type='checkbox' id='toggle-chart' />
-								<label htmlFor='toggle-chart' className='toggle' onClick={() => setShowChartLines(!showChartLines)}><span></span></label>
+								<div>Hide chart lines</div>
 							</div>
-							<div>Hide chart lines</div>
-						</div>
 
-						<div className='flex items-center'>
-							<div className='checkbox-wrapper mr-2'>
-								<input type='checkbox' id='imperial-conv' />
-								<label htmlFor='imperial-conv' className='toggle' onClick={() => setImperialConv(!imperialConv)}><span></span></label>
+							<div className='flex items-center'>
+								<div className='checkbox-wrapper mr-2'>
+									<input type='checkbox' id='imperial-conv' />
+									<label htmlFor='imperial-conv' className='toggle' onClick={() => setImperialConv(!imperialConv)}><span></span></label>
+								</div>
+								<div>Use freedom units</div>
 							</div>
-							<div>Use freedom units</div>
+
+							<About />
 						</div>
 
-						<About />
-					</div>
+						<Sort notSelected={notSelected} setSortedNotSelected={setSortedNotSelected} heightData={heightData} />
+						<Search setSearchStr={setSearchStr} />
 
-					<Sort notSelected={notSelected} setSortedNotSelected={setSortedNotSelected} heightData={heightData} />
-					<Search setSearchStr={setSearchStr} />
-
-					{sortedNotSelected.filter((name) => name.toLowerCase().includes(searchStr)).map((name, index) => (
-						<div
-							key={index}
-							className='flex justify-between items-center w-full gap-6 sm:gap-0 bg-gray-900 my-2 px-4 py-2 rounded-2xl text-lg'
-						>
+						{sortedNotSelected.filter((name) => name.toLowerCase().includes(searchStr)).map((name, index) => (
 							<div
-								className='w-10 h-10 sm:mr-2 bg-contain rounded-full border-2'
-								style={{'backgroundImage': `url(./${name.replaceAll(' ', '%20')}.png)`, 'borderColor': `#${heightData[name]['color']}`}}
-							></div>
-							<div className='font-bold grow whitespace-nowrap text-ellipsis overflow-hidden'>{name}</div>
-							<div className='font-bold sm:w-[20%]'>{!imperialConv ? heightData[name]['height'] : cmToFeetAndInches(heightData[name]['height'])}</div>
-							<button
-								className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
-								onClick={() => updateSelection(name, true, true)}
+								key={index}
+								className='flex justify-between items-center w-full gap-6 sm:gap-0 bg-gray-900 my-2 px-4 py-2 rounded-2xl text-lg'
 							>
-								<span className='material-symbols-outlined'>add</span>
-							</button>
-						</div>
-					))}
+								<div
+									className='w-10 h-10 sm:mr-2 bg-contain rounded-full border-2'
+									style={{'backgroundImage': `url(./${name.replaceAll(' ', '%20')}.png)`, 'borderColor': `#${heightData[name]['color']}`}}
+								></div>
+								<div className='font-bold grow whitespace-nowrap text-ellipsis overflow-hidden'>{name}</div>
+								<div className='font-bold sm:w-[20%]'>{!imperialConv ? heightData[name]['height'] : cmToFeetAndInches(heightData[name]['height'])}</div>
+								<button
+									className='flex justify-center items-center w-7 h-7 rounded-full hover:bg-gray-300 hover:text-gray-800 duration-100'
+									onClick={() => updateSelection(name, true, true)}
+								>
+									<span className='material-symbols-outlined'>add</span>
+								</button>
+							</div>
+						))}
+					</div>
 				</div>
-			</div>
 			</div>
     </>
   )
