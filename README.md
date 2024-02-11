18909b98356af5e44a911b2dcdfaad734c55c017
risbi0 <81689588+risbi0@users.noreply.github.com>
2024-02-11 17:12:34 +0800

    integrate image files

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ### Holo Heights App
 
-Made in React and Tailwind.
+Made in React and Tailwind. Images processed using Python.
+
+When adding new members:
+
+1.) Save image in `images/raw` folder.
+
+2.) Edit image, crop the bottom to reach the sole/feet and save.
+
+3.) Crop the top to reach the top of head and save to `images/ref`.
+
+4.) Add the appropriate info in `data` dict in `images/script.py` and run the script.
+
+5.) Add the appropriate info in `website/src/data.js`.
---
+++ website/src/Images.jsx
@@ -0,0 +1,17 @@
+function Images({selected, selectedImages}) {
+	return (
+		<>
+			{Object.entries(selected).map(([name, data], index) => (
+				<img
+					key={index}
+					ref={(r) => {selectedImages.current[index] = r}}
+					src={`https://raw.githubusercontent.com/risbi0/Holo-Heights-App/main/images/out/${name.replaceAll(' ', '_')}.png`}
+					className={`absolute bottom-0 left-1/2 h-full object-cover pointer-events-none ${!name['show'] ? '' : 'hidden'}`}
+					style={{'transform': data['transform'], 'zIndex': data['zIndex']}}
+				/>
+			))}
+		</>
+	)
+}
+
+export default Images
